7b83e19c1a0fdef69e71845e9184d2fae00da31a
hasanali207 <hasanali142228@gmail.com>
2024-11-08 18:29:43 +0600

    added problem2

--- dist/problem1.js
+++ dist/problem1.js
@@ -1,12 +1,11 @@
 "use strict";
 // Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
-{
-    function sumoOfArray(array) {
-        let sum = 0;
-        for (let i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-        return sum;
+function sumoOfArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
     }
-    const arr = [1, 2, 3, 4, 5];
+    return sum;
 }
+const sumArray = [1, 2, 3, 4];
+console.log(sumoOfArray(sumArray));
---
+++ problem/problem2.ts
@@ -0,0 +1,12 @@
+//Problem NO: 2
+// Create a TypeScript function removeDuplicates that accepts an array of numbers and returns a new array with duplicates removed. Preserve the original order of elements.
+
+const removeDuplicates = (arr: number[]): number[] => {
+  const newArray = arr.filter((item, index) => arr.indexOf(item) === index);
+
+  return newArray;
+};
+
+removeDuplicates([1, 2, 2, 3, 4, 4, 5]);
+
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]))
