62038586d1e66084034242c667f3e5bf099574ac
hasanali207 <hasanali142228@gmail.com>
2024-11-08 16:11:01 +0600

    problem1 added

--- dist/problem1.js
+++ dist/problem1.js
@@ -1,1 +1,12 @@
 "use strict";
+// Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+{
+    function sumoOfArray(array) {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+    const arr = [1, 2, 3, 4, 5];
+}
---
+++ problem/problem1.ts
@@ -0,0 +1,13 @@
+// Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+
+function sumoOfArray(arr: number[]): number {
+  let sum: number = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+const sumArray: number[] = [1, 2, 3, 4];
+
+// console.log(sumoOfArray(sumArray));
